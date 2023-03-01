6f20b19c6247b87f81f2323d5cccb1b1e70cbc00
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-03-01 14:44:24 +0300

    Some changes

--- 09_Classes/Program.cs
+++ 09_Classes/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using _09_Classes;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -32,28 +34,107 @@
         // bir tanesi öğrencinin verilerini ekrana döksün
         // bir tanesi de öğrencinin aldığı nota göre 50 kontrol değeri...geçti kaldı desin
 
-        clsOgrenci ogr1 = new clsOgrenci();
+        //clsOgrenci ogr1 = new clsOgrenci();
 
-        ogr1.OgrNo = 1;
-        ogr1.OgrAdSoyad = "Ümit Karaçivi";
-        ogr1.Ders = "Astroloji";
-        ogr1.DersNot = 35;
+        //ogr1.OgrNo = 1;
+        //ogr1.OgrAdSoyad = "Ümit Karaçivi";
+        //ogr1.Ders = "Astroloji";
+        //ogr1.DersNot = 35;
 
-        ogr1.OgrenciYaz();
+        //ogr1.OgrenciYaz();
 
-        ogr1.NotKontrol(ogr1.DersNot);
+        //ogr1.NotKontrol(ogr1.DersNot);
 
 
-        clsOgrenci ogr2 = new clsOgrenci();
+        //clsOgrenci ogr2 = new clsOgrenci();
 
-        ogr2.OgrNo = 2;
-        ogr2.OgrAdSoyad = "Bill Gates";
-        ogr2.Ders = "Girişimcilik";
-        ogr2.DersNot = 75;
+        //ogr2.OgrNo = 2;
+        //ogr2.OgrAdSoyad = "Bill Gates";
+        //ogr2.Ders = "Girişimcilik";
+        //ogr2.DersNot = 75;
 
-        ogr2.OgrenciYaz();
+        //ogr2.OgrenciYaz();
 
-        ogr2.NotKontrol(ogr2.DersNot);
+        //ogr2.NotKontrol(ogr2.DersNot);
+
+        #endregion
+
+        #region Personel örneği 2
+        // bu örneğin amacı class yapısının bu source dosyasından farklı bir yerde tanımlanması ve kullanılması üzerine
+
+        // Bir şirket var
+        // 5 elemanı var...Ad, soyad,ünvan,maas ve izingünü
+        // bir tane metodum olacak class içinde
+        // mu metotda öncelikle adsoyad bilgisinin dolu olduğu durumda
+        // maasının miktarına göre o perselin kaç gün izin hakkı olacağı belirlenecek
+        // kriterler
+        // maas < 2500 ise personelin ünvanının oluşturulamadığını belirten bir mesaj olabilir
+        // maas 2500 - 10000 kina arasındaysa junior personel ve izin gün 14
+        // maas 10001 - 14000 arasındaysa Kıdemli Personel ve izin günü 20 gün
+        // maas 14001 - 15500 arasındaysa Kıdemli+ Personel (sınırlar dahil) ve izin günü 24 gün
+        // maas 15500 üstündeyse Uzman Personel ve izin gün sayısı 30
+        // olarak çıkış veren şekilde olacak
+
+        // not : personelin maas bilgisinin belirlenmesi bilgisayara bırakılacak ...random...aralık 1000-20000 aralığı olsun..
+
+
+        // Not : eğer ad ve soyad bilgileri boş gelirse bir uyarı mesajı verilebilir
+
+        // nesnelerimizi sınıftan clsPersonel2 sınıfından oluşturuyoruz
+        clsPersonel2 ozcan = new clsPersonel2();
+        clsPersonel2 ertugrul = new clsPersonel2();
+        clsPersonel2 helin = new clsPersonel2();
+        clsPersonel2 berna = new clsPersonel2();
+        clsPersonel2 nergis = new clsPersonel2();
+        clsPersonel2 snur = new clsPersonel2();
+        clsPersonel2 mferit = new clsPersonel2();
+        clsPersonel2 nesibe = new clsPersonel2();
+        clsPersonel2 ozdenur = new clsPersonel2();
+        clsPersonel2 aleyna = new clsPersonel2();
+        clsPersonel2 ilker = new clsPersonel2();
+        clsPersonel2 busra = new clsPersonel2();
+        clsPersonel2 burak = new clsPersonel2();
+        clsPersonel2 mdogan = new clsPersonel2();
+        clsPersonel2 umit = new clsPersonel2();
+
+        // Bilgi doldurulması
+        int maasilk = 1000;
+        int maasson = 20000;
+
+        ozcan.setPersonelBilgileri("Özcan", "KARA", new Random().Next(maasilk, maasson));
+        ertugrul.setPersonelBilgileri("Ertuğrul", "DAĞLI", new Random().Next(maasilk, maasson));
+        helin.setPersonelBilgileri("Helin", "AYDIN", new Random().Next(maasilk, maasson));
+        berna.setPersonelBilgileri("Berna", "ELKOVAN", new Random().Next(maasilk, maasson));
+        nergis.setPersonelBilgileri("Nergis", "KETENCİ", new Random().Next(maasilk, maasson));
+        snur.setPersonelBilgileri("Saadet Nur", "KARAKAŞ", new Random().Next(maasilk, maasson));
+        mferit.setPersonelBilgileri("Muhammet Ferit", "ŞİMŞEK", new Random().Next(maasilk, maasson));
+        nesibe.setPersonelBilgileri("Nesibe", "KOSANOĞLU", new Random().Next(maasilk, maasson));
+        ozdenur.setPersonelBilgileri("Özdenur", "KOYUNCU", new Random().Next(maasilk, maasson));
+        aleyna.setPersonelBilgileri("Aleyna", "ÖZ", new Random().Next(maasilk, maasson));
+        ilker.setPersonelBilgileri("İlker", "KAPLAN", new Random().Next(maasilk, maasson));
+        busra.setPersonelBilgileri("Büşra", "KÖSE", new Random().Next(maasilk, maasson));
+        burak.setPersonelBilgileri("Burak", "ACAR", new Random().Next(maasilk, maasson));
+        mdogan.setPersonelBilgileri("Mehmet Doğan", "ERBAY", new Random().Next(maasilk, maasson));
+        umit.setPersonelBilgileri("Ümit", "KARAÇİVİ", new Random().Next(maasilk, maasson));
+
+        // Bilgilerin okunması
+        Console.WriteLine(ozcan.getPersonelBilgileri());
+        Console.WriteLine(ertugrul.getPersonelBilgileri());
+        Console.WriteLine(helin.getPersonelBilgileri());
+        Console.WriteLine(berna.getPersonelBilgileri());
+        Console.WriteLine(nergis.getPersonelBilgileri());
+        Console.WriteLine(snur.getPersonelBilgileri());
+        Console.WriteLine(mferit.getPersonelBilgileri());
+        Console.WriteLine(nesibe.getPersonelBilgileri());
+        Console.WriteLine(ozdenur.getPersonelBilgileri());
+        Console.WriteLine(aleyna.getPersonelBilgileri());
+        Console.WriteLine(ilker.getPersonelBilgileri());
+        Console.WriteLine(busra.getPersonelBilgileri());
+        Console.WriteLine(burak.getPersonelBilgileri());
+        Console.WriteLine(mdogan.getPersonelBilgileri());
+        Console.WriteLine(umit.getPersonelBilgileri());
+
+
 
         #endregion
 
---
+++ 09_Classes/clsPersonel2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Classes
+{
+    internal class clsPersonel2
+    {
+        // Değişgenler/Prop ların tanımlanması
+
+        public int PersonelID;
+        public string Ad;
+        public string Soyad;
+        public string Unvan = null;
+        public int Maas;
+        public int IzinGunu;
+
+        public void setPersonelBilgileri(string pAd,string pSoyad,int pMaas)
+        {
+            if (pAd.Length > 0 && pSoyad.Length>0) // ad ve soyadın tam girilmesi kontrol edilir
+            {
+                if (pMaas >= 2500)
+                {
+                    this.Ad = pAd;
+                    this.Soyad= pSoyad;
+                    this.Maas= pMaas;
+                    this.IzinGunu = 14;
+
+
+                    if (pMaas >= 10000 && pMaas <= 14000)
+                    {
+                        this.Unvan = "Kıdemli Personel";
+                        this.IzinGunu = 20;
+                    }
+                    else if (pMaas >= 14001 && pMaas <= 15500)
+                    {
+                        this.Unvan = "Kıdemli+ Personel";
+                        this.IzinGunu = 24;
+                    }
+                    else if (pMaas > 15500)
+                    {
+                        this.Unvan = "Uzman Personel";
+                        this.IzinGunu = 30;
+                    }
+                    else
+                        this.Unvan = "Junior Personel";
+                }
+                else
+                {
+                    Console.WriteLine("\nPersonel oluşturulamadı...");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("\nİsim bilgisi hatalıdır..kontrol ediniz");
+            }
+        }
+
+        public string getPersonelBilgileri()
+        {
+            if (Unvan != null)
+            {
+                return $"Personel Ad : {Ad}\nPersonel Soyad : {Soyad}\nÜnvan : {Unvan}\nMaas : {Maas} PNG Kina\nİzin Günü : {IzinGunu} gün\n";
+            }
+            else
+            {
+                return "Lütfen personeli oluşturunuz...";
+            }
+        }
+
+    }
+}

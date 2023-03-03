bf1bdeed64038725730fed55ffc80e6163cfbb11
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-03-03 09:03:41 +0300

    some

--- 13_Constructor/clsTest.cs
+++ 13_Constructor/clsTest.cs
@@ -12,6 +12,11 @@ namespace _13_Constructor
         public string s;// default = ""
         public bool b; // default= false
 
+        // Kural : class ismiyle aynı olacak bir metot aslında.
+        // ctor class devreye girdiğinde otomatik çalışan bir metotdur
+        // değişik durumlarda initial işlemler yapmak için kullanılır.
+
+        // parametreli de kullanımı olabilir.
         public clsTest()
         {
             i = 5;

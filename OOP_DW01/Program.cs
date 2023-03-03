855944bb9073b5b72260a94a256b7822d2626b74
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-03-03 15:09:13 +0300

    some changes

---
+++ OOP_DW01/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 1. Human adlı bir sınıf oluşturarak adınızı,iş tanımınızı,doğum tarihinizi oluşturunuz..İster ekrandan alarak ister hard coded olacak şekilde...
+        // tarihsel işlemler için C#'ın DateTime fonksiyonundan/metodundan yaralanabilirsiniz..
+
+
+        // Fruit isimli bir classınız daha olsun. Burada bir meyva tanımı (Elma)oluşturunuz...İster ekrandan alarak ister hard coded olacak şekilde...
+
+        // human sınıfından oluşan nesneniz Elma yesin ve bunu bir mesajla belirtiniz..
+
+
+        // Game isimli bir sınıfınız olsun. Human sınıfından oluşturulan bir nesneni için isteğiniz bir "....şu oyunu oynuyor..." diye bir mesaj verebilirsiniz...
+
+        // Human sınıfından birden fazla nesne üretebilirsiniz... 
+
+
+
+
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}

df48273ad467bb4c4b5f3fd5f17bcbf75d3ed58a
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-03-06 15:14:27 +0300

    Some changes

--- 16_Interfaces/Program.cs
+++ 16_Interfaces/Program.cs
@@ -1,7 +1,39 @@
-﻿internal class Program
+﻿using _16_Interfaces;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int option;
+
+        clsDbManager database;
+
+        ///---> Sorgulama İşlemleri
+        ///
+        Console.WriteLine("< Veritabanı Bağlantıları >\n");
+        Console.WriteLine("[1] - Oracle Database Bağlantısı Kur");
+        Console.WriteLine("[2] - Mysql Database Bağlantısı Kur");
+        Console.WriteLine("[3] - MongoDb Bağlantısı Kur\n");
+        Console.Write("-> Seçiminiz : ");
+
+        option = Convert.ToInt32(Console.ReadLine());
+
+        switch (option)
+        {
+            case 1:
+                database = new clsDbManager(new clsOracleDbManager());
+                database.loginDB();
+                break;
+            case 2:
+                database = new clsDbManager(new clsMySQLDbManager());
+                database.loginDB();
+                break;
+            case 3:
+                database = new clsDbManager(new clsMongoDbManager());
+                database.loginDB();
+                break;
+            default:
+                break;
+        }
     }
 }
